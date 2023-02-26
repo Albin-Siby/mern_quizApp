10201a80ebbcda12cdb1a77db46983a33c528403
Albin <albinsibi20417@gmail.com>
2023-02-26 12:43:15 +0530

    create models for routes

---
+++ server/models/questionSchema.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+const Schema = mongoose()
+
+const questionModel = new Schema({
+    questions : { type: Array, default: []},
+    answers : { type: Array, default: []},
+    createdAt : { type: Date, default: Date.now}
+});
+
+export default mongoose.model('Questions', questionModel)
